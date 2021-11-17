9eb12d871fd2045d62c25a2e639724c8edbd83e4
Dan Lin <no-reply-email>
2021-11-16 19:25:49 -0800

    Resolved gh-issue maybe
    Signed-off-by: Dan Lin <no-reply-email>

--- gh-pages.js
+++ gh-pages.js
@@ -1,17 +1,17 @@
 import { publish } from 'gh-pages';
 
 publish(
- 'build', // path to public directory
- {
-  branch: 'gh-pages',
-  repo: 'https://github.com/danlin604/hello-svelte.git',
-  user: {
-   name: 'Dan',
-   email: '' // Update to use your email
-  },
-  dotfiles: true
-  },
-  () => {
-   console.log('Deploy Complete!');
-  }
+	'build', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/danlin604/hello-svelte.git',
+		user: {
+			name: 'Dan',
+			email: '' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
 );
